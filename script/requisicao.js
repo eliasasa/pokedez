b271cf9d19b91d8ad30bd6bda68eb53a246df6f1
eliasasa <eliasnetoneto1@hotmail.com>
2025-02-03 20:37:45 -0400

    el soldado trifaldón

--- script/requisicao.js
+++ script/requisicao.js
@@ -8,6 +8,22 @@ async function sivenha(pokemon) {
     try {
         const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
         if (resposta.status === 404 && pokemon !== '') {
+            if (pokemon === 'miku') {
+                return {
+                    id: 'lepolepo',
+                    name: 'Hatsune Miku',
+                    sprites: { other: { home: { front_default: './images/others/miku.gif' } } },
+                    types: [{ type: { name: 'God' } }],
+                    stats: [
+                        { base_stat: 999 }, 
+                        { base_stat: 999 }, 
+                        { base_stat: 999 },
+                        { base_stat: 999 }, 
+                        { base_stat: 999 }, 
+                        { base_stat: 999 }  
+                    ]
+                };
+            }
             return null;
         } else if (!resposta.ok) {
             throw new Error(`Erro HTTP: ${resposta.status}`);
@@ -37,9 +53,9 @@ async function dados_basicos(element) {
 
         const capitalizedNome = maiuscula(pesquisar.name);
         document.getElementById('poke-nome').textContent = `${currentPokemonId} - ${capitalizedNome}`;
-        document.getElementById('sprite').src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pesquisar.id}.png`;
+        document.getElementById('sprite').src = pesquisar.sprites.other.home.front_default;
         document.getElementById('tipo').textContent = pesquisar.types.map(typeInfo => typeInfo.type.name).join(', ');
-        
+
         const stats = pesquisar.stats;
         document.getElementById('defesa').textContent = stats[2].base_stat;
         document.getElementById('speed').textContent = stats[5].base_stat;
@@ -55,6 +71,7 @@ async function dados_basicos(element) {
         searchBar.classList.add('search-bar-error');
     }
 }
+
 
 function diminuirValor() {
     if (currentPokemonId !== null && currentPokemonId > 1) {
@@ -73,3 +90,78 @@ function aumentarValor() {
         dados_basicos(searchBar);
     }
 }
+
+const songs = [
+    {src: './music/song1.mp3', name: 'Pokémon GO - Gym Leader Battle Music (HQ)'},
+    { src: './music/song2.mp3', name: '【ポケモン feat. 初音ミク】ミライどんなだろう ⧸ Mitchie M.mp3'},
+    { src: './music/song3.mp3', name: 'Pokémon Black & White - N Final Battle Music'},
+    { src: './music/song4.mp3', name: "Amidst the Grave's Demons - Suck My 401k ft. Chris Linck"},
+    { src: './music/song5.mp3', name: 'El Soldado Trifaldón'}
+];
+
+let musga = 0;
+
+const audioPlayer = document.getElementById('audio-player');
+const songTitle = document.getElementById('song-title');
+const musicPlayer = document.querySelector('.music-player');
+
+function prox() {
+    musga = (musga + 1) % songs.length;
+    updatePlayer();
+}
+
+function ante() {
+    musga = (musga - 1 + songs.length) % songs.length;
+    updatePlayer();
+}
+
+function updatePlayer() {
+    audioPlayer.src = songs[musga].src;
+    songTitle.textContent = `Tocando: ${songs[musga].name}`;
+    audioPlayer.play();
+    marquee();
+}
+
+function escondidinho() {
+    if (musicPlayer.classList.contains('show')) {
+        musicPlayer.classList.remove('show');
+    } else {
+        musicPlayer.classList.add('show');
+    }
+}
+
+function marquee() {
+    songTitle.style.animation = 'none';
+    setTimeout(function () {
+        songTitle.style.animation = '';
+    }, 10);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    audioPlayer.src = songs[musga].src;
+    songTitle.textContent = `Tocando: ${songs[musga].name}`;
+    musicPlayer.classList.remove('show');
+    audioPlayer.volume = 0.1;
+    
+    function playMusic() {
+        const playPromise = audioPlayer.play();
+        if (playPromise !== undefined) {
+            playPromise.then(() => {
+                console.log('A música começou a tocar automaticamente');
+            }).catch(error => {
+                console.log('A reprodução automática falhou:', error);
+            });
+        }
+        document.removeEventListener('click', playMusic);
+    }
+
+    document.addEventListener('click', playMusic);
+
+    audioPlayer.addEventListener('ended', prox); 
+});
+
+
+
+
+
+
